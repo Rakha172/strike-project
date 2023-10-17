ce3950d7dc67df7d5802df07c1447af0e633e65f
unknown <rhmakhrani@gmail.com>
2023-10-17 17:21:15 +0700

    first commit

---
+++ database/migrations/2023_10_17_053634_create_results_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('results', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('event_registration_id');
+            $table->foreign('event_registration_id')->references('id')->on('events_registration');
+            $table->string('berat_ikan');
+            $table->string('jenis_ikan');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('results');
+    }
+};
