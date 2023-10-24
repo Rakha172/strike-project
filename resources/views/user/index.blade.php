b1206691d52b02d057718c69f8a602b908ac7855
adamprojekbaru <adamrafifan@gmail.com>
2023-10-24 08:31:20 +0700

    userblade

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -1,58 +1,58 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!doctype html>
+    <html lang="en">
+    <head>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+        <!-- Bootstrap CSS -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
-    <title>Data Member</title>
-  </head>
-  <body>
-    @extends('componen.layout')
+        <title>Data Member</title>
+    </head>
+    <body>
+        @extends('componen.layout')
 
-    @section('content')
+        @section('content')
 
 
-    <div class="container">
-        <div class="card">
-                <h1 class="text-center fs-2 mt-4">DATA MEMBER</h1>
-            <div class="card-body">
+        <div class="container">
+            <div class="card">
+                    <h1 class="text-center fs-2 mt-4">DATA MEMBER</h1>
+                <div class="card-body">
 
-        @if ($pesan = session('berhasil'))
-        <div class="alert alert-primary" role="alert">
-           {{ $pesan }}
+            @if ($pesan = session('berhasil'))
+            <div class="alert alert-primary" role="alert">
+            {{ $pesan }}
+            </div>
+            @endif
+            <table class="table">
+                <thead>
+                <tr>
+                    <th scope="col">NO</th>
+                    <th scope="col">Name</th>
+                    <th scope="col">Email</th>
+                </tr>
+                </thead>
+                <tbody>
+                    @foreach ($user as $key => $usr)
+                <tr>
+                    <th scope="row">{{ $key + 1}}</th>
+                    <td>{{ $usr->name }}</td>
+                    <td>{{ $usr->email }}</td>
+                    <td>
+                    <td class="d-flex">
+                    </td>
+                </tr>
+                @endforeach
+                </tbody>
+            </table>
         </div>
-        @endif
-        <table class="table">
-            <thead>
-              <tr>
-                <th scope="col">NO</th>
-                <th scope="col">Name</th>
-                <th scope="col">Email</th>
-              </tr>
-            </thead>
-            <tbody>
-                @foreach ($user as $key => $usr)
-              <tr>
-                <th scope="row">{{ $key + 1}}</th>
-                <td>{{ $usr->name }}</td>
-                <td>{{ $usr->email }}</td>
-                <td>
-                <td class="d-flex">
-                </td>
-              </tr>
-              @endforeach
-            </tbody>
-          </table>
-    </div>
-    {{-- <td colspan="n">
-        <div style="text-align: center;"> --}}
+        {{-- <td colspan="n">
+            <div style="text-align: center;"> --}}
 
-    @endsection
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+        @endsection
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 
-  </body>
-</html>
+    </body>
+    </html>
