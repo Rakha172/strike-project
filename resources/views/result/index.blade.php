5d1be443963d195c0ea2e32834e76ab84a586c6c
unknown <rhmakhrani@gmail.com>
2023-10-24 14:03:50 +0700

    result

--- resources/views/result/index.blade.php
+++ resources/views/result/index.blade.php
@@ -8,12 +8,20 @@
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
-    <title>Data Resul</title>
+    <title>Results</title>
   </head>
   <body>
+    @extends('componen.layout')
+
+    @section('content')
+
+
     <div class="container">
-    <h2>Daftar Hasil Pemancingan</h2>
-    <a href="{{ route('result.create') }}" class="btn btn-primary">Tambah Hasil</a>
+        <div class="card">
+                <h1 class="text-center fs-2 mt-4">DATA RESULTS</h1>
+            <div class="card-body">
+
+    <a href="{{ route('result.create') }}" class="btn btn-dark">Tambah</a>
     <table class="table">
         <thead>
             <tr>
@@ -52,6 +60,8 @@
         </tbody>
     </table>
 </div>
+
+@endsection
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 
 </body>
