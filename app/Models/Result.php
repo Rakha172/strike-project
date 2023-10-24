bae205dc9eebf28bf300a67d4831838f410e999b
sintadla <sintauser817@gmail.com>
2023-10-24 10:51:42 +0700

    controller/model

---
+++ app/Models/Result.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\Event_Registration;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+
+class Result extends Model
+{
+    use HasFactory;
+    protected $table = 'results';
+
+    protected $fillable = [
+        'event_id',
+        'user_id',
+        'events_registration_id',
+        'fish_count',
+        'weight',
+        'status',
+    ];
+
+    public function event()
+    {
+        return $this->belongsTo(Event::class, 'event_id');
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function eventRegistration()
+    {
+        return $this->belongsTo(Event_Registration::class, 'events_registration_id');
+    }
+}
