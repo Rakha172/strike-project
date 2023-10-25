2f969e0556542d37fdd29051d6d4fd37780f5993
unknown <rhmakhrani@gmail.com>
2023-10-25 10:11:21 +0700

    edit

--- resources/views/event/edit.blade.php
+++ resources/views/event/edit.blade.php
@@ -71,14 +71,6 @@
                   @enderror
 
               </div>
-              <div class="mb-3">
-                <label class="form-label">Category</label>
-                <input value="{{ old('category', $events->category) }}" name="category" type="text" class="form-control @error('category') is-invalid @enderror">
-                  @error('category')
-                      <div class="invalid-feedback">{{ $message }}</div>
-                  @enderror
-
-              </div>
             <button type="submit" class="btn btn-dark">Submit</button>
           </form>
     </div>
