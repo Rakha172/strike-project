e3d8cf0995bf203e227011c3fc4ad1e40cdc09ac
unknown <rhmakhrani@gmail.com>
2023-10-25 08:21:11 +0700

    dashboard

--- resources/views/dashboard/dashboard.blade.php
+++ resources/views/dashboard/dashboard.blade.php
@@ -22,9 +22,9 @@
             <li><a href="{{ route('event.index')}}"><i class='bx bx-store-alt'></i>Event</a></li>
             <li><a href="{{ route('setting.index')}}"><i class='bx bx-cog'></i>Setting</a></li>
             <li><a href="{{ route('user.index')}}"><i class='bx bx-user'></i>Data Member</a></li>
+            <li><a href="{{ route('event_registration.index')}}"><i class='bx bx-user'></i>Data EventRegist</a></li>
             <li><a href="{{ route('chart.index')}}"><i class='bx bx-pie-chart'></i>Grafik</a></li>
-            <li><a href="{{ route('result.index')}}"><i class='bx bx-user'></i>Result</a></li>
-            <li><a href="{{ route('event_registration.index')}}"><i class='bx bx-user'></i>EventRegis</a></li>
+            <li><a href="{{ route('result.index')}}"><i class='bx bx-line-chart'></i>Result</a></li>
         </ul>
         <ul class="side-menu">
             <li>
