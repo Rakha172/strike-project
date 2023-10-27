a48cf0b90bd35999fd014086303c6b12fc0a410f
unknown <rhmakhrani@gmail.com>
2023-10-27 10:53:01 +0700

    database2

--- database/migrations/2023_10_24_014340_create_setting_table.php
+++ database/migrations/2023_10_24_014340_create_setting_table.php
@@ -13,7 +13,7 @@ return new class extends Migration {
         Schema::create('setting', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('history');
+            $table->text('history');
             $table->string('location');
             $table->string('image');
             $table->timestamps();
