ec55897efbb98abf2d51f1d3b0b6cee116d7c029
unknown <rhmakhrani@gmail.com>
2023-10-30 09:57:19 +0700

    blade3

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -1,36 +1,15 @@
-@extends('componen.layout')
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    {{-- <title>Add Events</title> --}}
+  </head>
+  <body>
+    @extends('componen.layout')
+
 @section('content')
-<style>
-.container {
-    margin-top: 20px;
-    padding: 20px;
-}
-
-.card h1 {
-    text-align: center;
-}
-
-.alert.alert-primary {
-    margin-bottom: 20px;
-    background-color: #007bff;
-    color: #fff;
-}
-
-.table {
-    width: 100%;
-}
-
-.table thead {
-    background-color: #007bff;
-    color: #fff;
-}
-
-.table th {
-    text-align: center;
-}
-
-</style>
-
 
         <div class="container">
             <div class="card">
