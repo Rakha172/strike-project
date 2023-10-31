556fd654fa4c68a3042e88c08d7308d69986e945
unknown <rhmakhrani@gmail.com>
2023-10-31 08:52:25 +0700

    blade5

--- resources/views/result/index.blade.php
+++ resources/views/result/index.blade.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
+<head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -9,8 +9,8 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
     <title>Results</title>
-  </head>
-  <body>
+</head>
+<body>
     @extends('componen.layout')
 
     @section('content')
@@ -36,7 +36,7 @@
                             <td>{{ $loop->iteration }}</td>
                             <td>{{ $result->user->name }}</td>
                             {{-- <td>{{ $result->eventRegistration->event->event_name }}</td> --}}
-                            <td>{{ $result->weight }} kg</td>
+                            <td>{{ $result->weight }}</td>
                             <td>{{ $result->status }}</td>
                             <td>
                                 <form action="{{ route('result.destroy', $result->id) }}" method="POST">
@@ -55,5 +55,5 @@
     @endsection
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-  </body>
+</body>
 </html>
