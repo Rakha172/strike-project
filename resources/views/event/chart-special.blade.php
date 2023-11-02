b2f2b83090a77ae9dbf90bcac30b7ff62d03c2a3
unknown <rhmakhrani@gmail.com>
2023-11-02 15:48:49 +0700

    blade3

---
+++ resources/views/event/chart-special.blade.php
@@ -0,0 +1,37 @@
+@extends('componen.layout')
+
+@section('content')
+<div style="display: flex; justify-content: flex-start;">
+    <div style="flex: 1;">
+        <canvas id="myChart"></canvas>
+    </div>
+</div>
+
+<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+<script>
+    var labels = @json($labels);
+    var data = @json($data);
+
+    var ctx = document.getElementById('myChart').getContext('2d');
+    var myChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: labels,
+            datasets: [{
+                label: 'Ikan Specials',
+                data: data,
+                backgroundColor: '#000080', // Navy
+                borderColor: '#000080',
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    });
+</script>
+@endsection
