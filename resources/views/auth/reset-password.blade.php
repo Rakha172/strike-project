4490449dcf74cca8702936510a669ac42af4a659
azzam <azzamnoya123@gmail.com>
2023-11-15 13:25:03 +0700

    index reset password

--- resources/views/auth/reset-password.blade.php
+++ resources/views/auth/reset-password.blade.php
@@ -1,65 +1,93 @@
 <!doctype html>
 <html lang="en">
+
 <head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Reset Password</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-  <!-- Tambahkan Bootstrap CSS -->
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    {{-- font awesome --}}
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
+        integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link href="{{ asset('css/login.css') }}" rel="stylesheet">
 
+    {{-- CSS Toastr Link --}}
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css"
+        integrity="sha512-vKMx8UnXk60zUwyUnUPM3HbQo8QfmNx7+ltw8Pm5zLusl1XIfwcxo8DbWCqMGKaWeNxWA8yrx5v3SaVpMvR3CA=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    {{-- Cdn Jquery --}}
+    <script src="https://code.jquery.com/jquery-3.7.0.min.js"
+        integrity="sha256-2Pmvv0kuTBOenSvLm6bvfBSSHrUJ+3A7x6P5Ebd07/g=" crossorigin="anonymous"></script>
 </head>
+    {{-- JS Toastr Link --}}
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"
+    integrity="sha512-VEd+nq25CkR676O+pLBnDW09R7VQX9Mdiij052gVCp5yVH3jGtH70Ho/UUv4mJDsEdTvqRCFZg0NKGiojGnUCw=="
+    crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
 <body>
-  <div class="container">
-    <div class="confirm-wrap p-4 p-md-5">
-      <div class="overlap">
-        <div class="w-1">
-          <h3 class="mb-4 text-center">RESET<br>PASSWORD</h3>
-          <p class="mb-4 text-center">Buatlah password baru Anda!</p>
+    <div class="overlay"></div>
+    <form action="{{ route('password.email') }}" method="POST" class="box">
+        @csrf
+        <div class="header">
+            <h4>Reset Password</h4>
+            <p>Masukan Masukan Password baru</p>
         </div>
-
-        <form action="{{ route('password.update', ['token' => $token ]) }}" method="POST" enctype="multipart/form-data">
-          @csrf
-          <input type="hidden" name="token" value="{{request()->token}}">
-          <input type="hidden" name="email" value="{{request()->email}}">
-
-          <div class="overlap-1">
-            <div class="rectangle">
-              <div class="form-floating mb-3">
-                <input type="password" id="floatingInput" name="password" class="form-control bg-light @error('password') is-invalid @enderror with-image" placeholder="Password">
-                <label for="floatingInput">New Password</label>
-                @error('name')
+        <div class="login area">
+            <script>
+                @if (Session::has('success'))
+                    toastr.info("{{ Session::get('success') }}", "", {
+                        positionClass: "toast-top-center"
+                    });
+                @elseif (Session::has('failed'))
+                    toastr.error("{{ Session::get('error') }}", "Oops!", {
+                    });
+                @endif
+            </script>
+            <input type="email" name="email"  class="email @error('email') is-invalid @enderror" value="{{ old('email') }}" placeholder="Masukkan password baru" required>
+            @error('email')
                 <div class="invalid-feedback text-start">
-                  {{ $message }}
+                    {{ $message }}
                 </div>
-                @enderror
-                <img src="{{ asset('img/login/padlock-1.png') }}" alt="" class="input-image">
-              </div>
+            @enderror
+            <div class="form-group">
+                <input type="submit" value="Submit" class="submit1">
             </div>
-          </div>
+            <a href="{{ route('login') }}">Back to login</a>
+    </form>
 
-          <div class="overlap-2">
-            <div class="rectangle">
-              <div class="form-floating mb-3">
-                <input type="password" id="floatingInput" name="password_confirmation" class="form-control bg-light @error('password') is-invalid @enderror" placeholder="Password">
-                <label for="floatingInput">Confirm New Password</label>
-                @error('password')
-                <div class="invalid-feedback text-start">
-                  {{ $message }}
-                </div>
-                @enderror
-              </div>
-            </div>
-          </div>
-          <div class="form-group">
-            <input type="submit" value="Reset Password" class="form-control btn btn-success rounded submit px-3">
-          </div>
-        </form>
-      </div>
     </div>
-  </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"
+        integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js"
+        integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous">
+    </script>
+    <script>
+        function showHidePassword() {
+            let passCheck = document.getElementById("password");
+            let show = document.getElementById("show");
+            let hide = document.getElementById("hide");
 
-  <!-- Tambahkan Bootstrap JavaScript (jika diperlukan) -->
-
+            if (passCheck.type == 'password') {
+                passCheck.type = "text";
+                show.style.display = "block";
+                show.style.display = "block";
+                hide.style.display = "none";
+            } else {
+                passCheck.type = "password";
+                show.style.display = "none";
+                hide.style.display = "block";
+            }
+        }
+    </script>
 </body>
+
 </html>
