9b8ddcdc7d75c07ef620ad3efe539a4be27d1a39
unknown <rhmakhrani@gmail.com>
2023-11-16 14:40:32 +0700

    public

--- resources/views/event/chart-result-and-total.blade.php
+++ resources/views/event/chart-result-and-total.blade.php
@@ -5,44 +5,109 @@
 <div style="display: flex; justify-content: flex-start;">
     <div style="flex: 1;">
         <canvas id="myChart"></canvas>
+        <div id="chart"></div>
     </div>
 </div>
 
-<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+<!-- Tambahkan skrip Chart.js dan plugin datalabels -->
+{{-- <script src="https://cdn.jsdelivr.net/npm/chart.js"></script> --}}
+<script src="https://cdn.jsdelivr.net/npm/apexcharts"></script>
+
+{{-- <script src="https://cdn.jsdelivr.net/npm/chartjs-plugin-datalabels"></script> --}}
+
 <script>
     var labels = @json($labels);
     var dataWeightTotal = @json($dataWeightTotal);
-    var dataTotalIkan = @json($dataTotalIkan);
+    var dataTotalIkan = @json($dataTotalIkan); // Variabel untuk data "jumlah ikan"
 
-    var ctx = document.getElementById('myChart').getContext('2d');
-    var myChart = new Chart(ctx, {
-        type: 'bar',
-        data: {
-            labels: labels,
-            datasets: [
-                {
-                    label: 'Weight Total',
-                    data: dataWeightTotal,
-                    backgroundColor: '#FF0000', // Red
-                    borderColor: '#FF0000',
-                    borderWidth: 1
-                },
-                {
-                    label: 'Jumlah Ikan',
-                    data: dataTotalIkan,
-                    backgroundColor: '#008000', // green
-                    borderColor: '#008000',
-                    borderWidth: 1
-                },
-            ]
+    var options = {
+    series: [
+        {
+            name: 'Weight Total',
+            data: dataWeightTotal,
         },
-        options: {
-            scales: {
-                y: {
-                    beginAtZero: true
-                }
-            }
+        {
+            name: 'Jumlah Ikan', // Nama series untuk data "jumlah ikan"
+            data: dataTotalIkan, // Masukkan data "jumlah ikan" ke dalam series
         }
-    });
+    ],
+          chart: {
+          height: 350,
+          type: 'bar',
+        },
+        plotOptions: {
+          bar: {
+            borderRadius: 10,
+            dataLabels: {
+              position: 'top', // top, center, bottom
+            },
+          }
+        },
+        dataLabels: {
+          enabled: true,
+          formatter: function (val) {
+            return val;
+          },
+          offsetY: -20,
+          style: {
+            fontSize: '12px',
+            colors: ["#304758"]
+          }
+        },
+
+        xaxis: {
+          categories: labels,
+          position: 'top',
+          axisBorder: {
+            show: false
+          },
+          axisTicks: {
+            show: false
+          },
+          crosshairs: {
+            fill: {
+              type: 'gradient',
+              gradient: {
+                colorFrom: '#D8E3F0',
+                colorTo: '#BED1E6',
+                stops: [0, 100],
+                opacityFrom: 0.4,
+                opacityTo: 0.5,
+              }
+            }
+          },
+          tooltip: {
+            enabled: true,
+          }
+        },
+        yaxis: {
+          axisBorder: {
+            show: false
+          },
+          axisTicks: {
+            show: false,
+          },
+          labels: {
+            show: false,
+            formatter: function (val) {
+              return val;
+            }
+          }
+
+        },
+        title: {
+          text: '',
+          floating: true,
+          offsetY: 330,
+          align: 'center',
+          style: {
+            color: '#444'
+          }
+        }
+    };
+
+    var chart = new ApexCharts(document.querySelector("#chart"), options);
+    chart.render();
+
 </script>
 @endsection
