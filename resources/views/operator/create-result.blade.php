4e730505a949034cf6dbbc208490e1c8645670ef
unknown <rhmakhrani@gmail.com>
2023-11-20 14:31:13 +0700

    views

--- resources/views/operator/create-result.blade.php
+++ resources/views/operator/create-result.blade.php
@@ -6,60 +6,132 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>{{$title->name}} | Add Result</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <style>
+        body {
+            background-color: #c5e4f3; /* Warna latar belakang biru muda */
+        }
+        .container {
+            background-color: #fff; /* Warna latar belakang putih untuk konten */
+            border-radius: 10px; /* Corner radius untuk container */
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1); /* Shadow efek pada container */
+            padding: 20px;
+            margin-top: 20px;
+        }
+        .card-kamera {
+            background-color: #fff; /* Warna latar belakang putih untuk card kamera */
+            border-radius: 10px; /* Corner radius untuk card */
+            box-shadow: 0 0 5px rgba(0, 0, 0, 0.1); /* Shadow efek pada card */
+            padding: 20px;
+        }
+        .photo-section {
+            padding: 20px;
+        }
+        .form-section {
+            padding: 20px;
+        }
+        #capturedImage {
+            max-width: 100%;
+            display: none;
+        }
+    </style>
 </head>
 <body>
 
     <div class="container mt-5">
         <div class="card">
             <div class="card-body">
-                <h1 class="text-center fs-2 mt-4" style="background-color:#c5e4f3;">Hasil Result Pemancingan</h1>
+                <h1 class="text-center fs-2 mt-4">Hasil Result Pemancingan</h1>
                 <h2 class="text-center fs-3 mt-4">{{ $event->name }}</h2>
 
-                <form action="{{ route("resultop.store", ['event' => $event->id]) }}" method="POST">
-                    @csrf
-                    @method("POST")
-                    <div class="form-group">
-                        <div class="form-group">
-                            <div class="form-group">
-                                <div class="mb-3">
-                                    <label for="participant">Participant</label>
-                                    <select name="participant" class="form-control" style="background-color:#c5e4f3;">
-                                        @foreach($event_registration as $eventReg)
-                                            <option value="{{ $eventReg->user_id }}">{{ $eventReg->user->name }}</option>
-                                        @endforeach
-                                    </select>
-                                </div>
-
-                                <div class="mb-3">
-                                    <label class="form-label">Berat Ikan</label>
-                                    <input value="{{ old('weight')}}" name="weight" type="number" class="form-control @error('weight') is-invalid @enderror" style="background-color:#c5e4f3;">
-                                      @error('weight')
-                                          <div class="invalid-feedback">{{ $message }}</div>
-                                      @enderror
-                                  </div>
-
-                                <div class="mb-3">
-                                    <label class="form-label">Status</label>
-                                    <select name="status" id="status" class="form-control" style="background-color:#c5e4f3;">
-                                        <option value="special">Special</option>
-                                        <option value="regular">Regular</option>
-                                    </select>
-                                </div>
-
-                                <button type="submit" class="btn btn-primary">Simpan</button>
+                <div class="row">
+                    <div class="col-md-4">
+                        <div class="card card-kamera">
+                            <div class="card-body">
+                                <h3 class="card-title">Ambil Foto</h3>
+                                <video id="cameraFeed" width="100%" height="auto" autoplay></video>
+                                <canvas id="canvas" style="display: none;"></canvas>
+                                <img id="capturedImage" src="#" alt="Captured Image">
                             </div>
-                </form>
+                        </div>
+                    </div>
+
+                    <div class="col-md-8">
+                        <div class="card">
+                            <div class="card-body">
+                                <form action="{{ route("resultop.store", ['event' => $event->id]) }}" method="POST">
+                                    @csrf
+                                    @method("POST")
+                                    <div class="form-group">
+                                        <label for="participant">Participant</label>
+                                        <select name="participant" class="form-control">
+                                            @foreach($event_registration as $eventReg)
+                                                <option value="{{ $eventReg->user_id }}">{{ $eventReg->user->name }}</option>
+                                            @endforeach
+                                        </select>
+                                    </div>
+
+                                    <div class="form-group">
+                                        <label class="form-label">Berat Ikan</label>
+                                        <input value="{{ old('weight')}}" name="weight" type="number" class="form-control @error('weight') is-invalid @enderror">
+                                        @error('weight')
+                                            <div class="invalid-feedback">{{ $message }}</div>
+                                        @enderror
+                                    </div>
+
+                                    <div class="form-group">
+                                        <label class="form-label">Status</label>
+                                        <select name="status" id="status" class="form-control">
+                                            <option value="special">Special</option>
+                                            <option value="regular">Regular</option>
+                                        </select>
+                                    </div>
+                                    <input type="hidden" name="image_data" id="imageData" value="">
+                                    <button onclick="capturePhoto()" class="btn btn-primary mt-3"> Simpan</button>
+                                </form>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
-            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-        </body>
-        </html>
         </div>
     </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+    <script>
+        async function setupCamera() {
+            const constraints = {
+                video: { width: 400, height: 300 }
+            };
+
+            const video = document.getElementById('cameraFeed');
+            try {
+                const stream = await navigator.mediaDevices.getUserMedia(constraints);
+                video.srcObject = stream;
+            } catch (err) {
+                console.error('Error accessing the camera:', err);
+            }
+        }
+
+        function capturePhoto() {
+            const video = document.getElementById('cameraFeed');
+            const canvas = document.getElementById('canvas');
+            const photo = document.getElementById('capturedImage');
+            const imageDataInput = document.getElementById('imageData');
+
+            canvas.width = 400;
+            canvas.height = 300;
+            canvas.getContext('2d').drawImage(video, 0, 0, canvas.width, canvas.height);
+
+            const imageData = canvas.toDataURL('image/png');
+            photo.setAttribute('src', imageData);
+            photo.style.display = 'block';
+
+            imageDataInput.value = imageData;
+            document.querySelector('form').submit();
+        }
+
+        setupCamera();
     </script>
 </body>
-
 </html>
