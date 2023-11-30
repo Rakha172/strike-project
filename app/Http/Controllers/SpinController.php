83020384b4d170589993aeb6e6d586eb3c4df1a4
unknown <rhmakhrani@gmail.com>
2023-11-30 14:19:27 +0700

    contro

--- app/Http/Controllers/SpinController.php
+++ app/Http/Controllers/SpinController.php
@@ -8,15 +8,15 @@ use Illuminate\Http\Request;
 class SpinController extends Controller
 {
     public function spin()
-{
-    // Mendapatkan angka-angka dari controller (gantilah dengan logika atau data sebenarnya)
-    $numbers = range(1, 10);
+    {
+        // Mendapatkan angka-angka dari controller (gantilah dengan logika atau data sebenarnya)
+        $numbers = range(1, 10);
 
-    // Dapatkan data acara dengan nilai "both" acak
-    $events = Event::inRandomOrder()->get();
+        // Dapatkan data acara dengan nilai "both" acak
+        $events = Event::inRandomOrder()->get();
 
-    // Kirimkan data acara dan angka-angka ke tampilan "spin"
-    return view('spin.spin', compact('events', 'numbers'));
-}
+        // Kirimkan data acara dan angka-angka ke tampilan "spin"
+        return view('spin.spin', compact('events', 'numbers'));
+    }
 
 }
