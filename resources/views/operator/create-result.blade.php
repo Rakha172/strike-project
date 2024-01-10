4dcc13b5e49df052efa75c2ef39c37c34bbb9cb5
unknown <rhmakhrani@gmail.com>
2024-01-10 16:09:23 +0700

    views3

--- resources/views/operator/create-result.blade.php
+++ resources/views/operator/create-result.blade.php
@@ -7,39 +7,6 @@
     <title>{{ $title->name }} | Add Result</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <style>
-        body {
-            background-color: #2D9596;
-        }
-
-        .container {
-            background-color: #ffffff;
-            border-radius: 10px;
-            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-            padding: 20px;
-            margin-top: 20px;
-        }
-
-        .card-kamera {
-            background-color: #2D9596;
-            border-radius: 10px;
-            box-shadow: 0 0 5px rgba(0, 0, 0, 0.1);
-            padding: 20px;
-        }
-
-        .photo-section {
-            padding: 20px;
-        }
-
-        .form-section {
-            padding: 20px;
-        }
-
-        #capturedImage {
-            max-width: 100%;
-            display: none;
-        }
-    </style>
 </head>
 
 <body>
