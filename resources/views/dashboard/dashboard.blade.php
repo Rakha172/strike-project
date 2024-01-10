dbe70add90626eec02254f3836c3fba208d4b9b7
unknown <rhmakhrani@gmail.com>
2024-01-10 16:08:07 +0700

    views2

--- resources/views/dashboard/dashboard.blade.php
+++ resources/views/dashboard/dashboard.blade.php
@@ -31,15 +31,20 @@
             <div class="logo-name"><span>Project</span>Strike</div>
         </a>
         <ul class="side-menu">
-            <li class="active"><a href="#"><i class='bx bxs-dashboard'></i>Dashboard</a></li>
-            <li><a href="{{ route('event.index')}}"><i class='bx bx-store-alt'></i>Event</a></li>
-            <li><a href="{{ route('setting.index')}}"><i class='bx bx-cog'></i>Setting</a></li>
-            <li><a href="{{ route('user.index')}}"><i class='bx bx-user'></i>Member</a></li>
-            <li><a href="{{ route('event_registration.index')}}"><i class='bx bx-user'></i>EventRegist</a></li>
-            <li><a href="{{ route('payment.index')}}"><i class='bx bx-dollar'></i>Payment-Confirm</a></li>
-            <li class="{{ request()->is('payment*') ? 'active' : '' }}">
-                <a href="{{ route('paymenttypesIndex') }}"><i class='bx bx-dollar'></i>Payment-Types</a>
-            </li>
+            @if (Auth::check())
+                @if (Auth::user()->role === 'admin')
+                    <li class="active"><a href="#"><i class='bx bxs-dashboard'></i>Dashboard</a></li>
+                    <li><a href="{{ route('event.index') }}"><i class='bx bx-store-alt'></i>Event</a></li>
+                    <li><a href="{{ route('setting.index') }}"><i class='bx bx-cog'></i>Setting</a></li>
+                    <li><a href="{{ route('user.index') }}"><i class='bx bx-user'></i>Member</a></li>
+                    <li><a href="{{ route('event_registration.index') }}"><i class='bx bx-user'></i>EventRegist</a></li>
+                    <li class="{{ request()->is('payment*') ? 'active' : '' }}">
+                        <a href="{{ route('paymenttypesIndex') }}"><i class='bx bx-dollar'></i>Payment-Types</a>
+                    </li>
+                @elseif(Auth::user()->role === 'operator')
+                    <li><a href="{{ route('eventsop.index') }}"><i class='bx bx-wrench'></i>Operator</a></li>
+                @endif
+            @endif
         </ul>
         <ul class="side-menu">
             <li>
@@ -71,12 +76,12 @@
             </a> --}}
             <a href="#" class="profile">
                 @php
-                $user = Auth::user();
-                $namaPengguna = $user->name;
-                $inisial = $namaPengguna[0];
+                    $user = Auth::user();
+                    $namaPengguna = $user->name;
+                    $inisial = $namaPengguna[0];
                 @endphp
-            <div class="profile-avatar">{{ $inisial }}</div>
-        </a>
+                <div class="profile-avatar">{{ $inisial }}</div>
+            </a>
         </nav>
 
         <!-- End of Navbar -->
@@ -87,11 +92,9 @@
                     <h1>Dashboard</h1>
                     <script>
                         @if (Session::has('success'))
-                            toastr.info("{{ Session::get('success') }}", "{{ Auth::user()->name }}", {
-                            });
+                            toastr.info("{{ Session::get('success') }}", "{{ Auth::user()->name }}", {});
                         @elseif (Session::has('failed'))
-                            toastr.error("{{ Session::get('error') }}", "Oops!", {
-                            });
+                            toastr.error("{{ Session::get('error') }}", "Oops!", {});
                         @endif
                     </script>
                     <ul class="breadcrumb">
@@ -148,9 +151,9 @@
 
 
 
-            </div>
+    </div>
 
-        </main>
+    </main>
 
     </div>
 
@@ -158,60 +161,60 @@
     <script>
         const sideLinks = document.querySelectorAll('.sidebar .side-menu li a:not(.logout)');
 
-sideLinks.forEach(item => {
-    const li = item.parentElement;
-    item.addEventListener('click', () => {
-        sideLinks.forEach(i => {
-            i.parentElement.classList.remove('active');
-        })
-        li.classList.add('active');
-    })
-});
+        sideLinks.forEach(item => {
+            const li = item.parentElement;
+            item.addEventListener('click', () => {
+                sideLinks.forEach(i => {
+                    i.parentElement.classList.remove('active');
+                })
+                li.classList.add('active');
+            })
+        });
 
-const menuBar = document.querySelector('.content nav .bx.bx-menu');
-const sideBar = document.querySelector('.sidebar');
+        const menuBar = document.querySelector('.content nav .bx.bx-menu');
+        const sideBar = document.querySelector('.sidebar');
 
-menuBar.addEventListener('click', () => {
-    sideBar.classList.toggle('close');
-});
+        menuBar.addEventListener('click', () => {
+            sideBar.classList.toggle('close');
+        });
 
-const searchBtn = document.querySelector('.content nav form .form-input button');
-const searchBtnIcon = document.querySelector('.content nav form .form-input button .bx');
-const searchForm = document.querySelector('.content nav form');
+        const searchBtn = document.querySelector('.content nav form .form-input button');
+        const searchBtnIcon = document.querySelector('.content nav form .form-input button .bx');
+        const searchForm = document.querySelector('.content nav form');
 
-searchBtn.addEventListener('click', function (e) {
-    if (window.innerWidth < 576) {
-        e.preventDefault;
-        searchForm.classList.toggle('show');
-        if (searchForm.classList.contains('show')) {
-            searchBtnIcon.classList.replace('bx-search', 'bx-x');
-        } else {
-            searchBtnIcon.classList.replace('bx-x', 'bx-search');
-        }
-    }
-});
+        searchBtn.addEventListener('click', function(e) {
+            if (window.innerWidth < 576) {
+                e.preventDefault;
+                searchForm.classList.toggle('show');
+                if (searchForm.classList.contains('show')) {
+                    searchBtnIcon.classList.replace('bx-search', 'bx-x');
+                } else {
+                    searchBtnIcon.classList.replace('bx-x', 'bx-search');
+                }
+            }
+        });
 
-window.addEventListener('resize', () => {
-    if (window.innerWidth < 768) {
-        sideBar.classList.add('close');
-    } else {
-        sideBar.classList.remove('close');
-    }
-    if (window.innerWidth > 576) {
-        searchBtnIcon.classList.replace('bx-x', 'bx-search');
-        searchForm.classList.remove('show');
-    }
-});
+        window.addEventListener('resize', () => {
+            if (window.innerWidth < 768) {
+                sideBar.classList.add('close');
+            } else {
+                sideBar.classList.remove('close');
+            }
+            if (window.innerWidth > 576) {
+                searchBtnIcon.classList.replace('bx-x', 'bx-search');
+                searchForm.classList.remove('show');
+            }
+        });
 
-const toggler = document.getElementById('theme-toggle');
+        const toggler = document.getElementById('theme-toggle');
 
-toggler.addEventListener('change', function () {
-    if (this.checked) {
-        document.body.classList.add('dark');
-    } else {
-        document.body.classList.remove('dark');
-    }
-});
+        toggler.addEventListener('change', function() {
+            if (this.checked) {
+                document.body.classList.add('dark');
+            } else {
+                document.body.classList.remove('dark');
+            }
+        });
     </script>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 
