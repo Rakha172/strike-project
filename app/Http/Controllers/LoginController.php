a2ce2c917b296efe375ca52d097fa4fee6ec44f7
rafly <oktorafly1@gmail.com>
2024-01-11 08:20:37 +0700

    login  with email or phone_number

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Setting;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Http;
@@ -29,11 +30,32 @@ class LoginController extends Controller
         $endpoint = $setting->endpoint;
 
         $credentials = $request->validate([
-            'email' => 'required|email|exists:users,email',
+            'email_or_phone_number' => 'required',
             'password' => 'required',
         ]);
 
-        $credentials = $request->only('email', 'password');
+        // check apakah input data berupa email
+        $isEmail = \filter_var($credentials['email_or_phone_number'], FILTER_VALIDATE_EMAIL);
+
+        // jika email, cek apakah email ada
+        if ($isEmail)
+        {
+            $credentials = $this->emailIsExist($credentials['email_or_phone_number']);
+        } else {
+            // jika bukan email, cek apakah nomor hp ada
+            $credentials = $this->phoneNumberIsExist($credentials['email_or_phone_number']);
+        }
+
+        if (!$credentials)
+        {
+            return back()->withErrors(['otp' => 'the email or password you entered is incorrect.'])->withInput();
+        }
+
+        $credentials = [
+            'email' => $credentials->email,
+            'password' => $request->post('password')
+        ];
+
         $remember = $request->has('remember');
 
         if (Auth::guard('web')->attempt($credentials, $remember)) {
@@ -72,6 +94,18 @@ class LoginController extends Controller
         } else {
             return back()->withErrors(['otp' => 'the email or password you entered is incorrect.'])->withInput();
         }
+    }
+
+    protected function phoneNumberIsExist($phoneNumber): User | null
+    {
+        $user = User::where('phone_number', $phoneNumber)->first();
+        return $user;
+    }
+
+    protected function emailIsExist(string $email): User | null
+    {
+        $user = User::where('email', $email)->first();
+        return $user;
     }
 
     public function logout()
