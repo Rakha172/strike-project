43123456dd3b91b9731eca3ccb786c7d173b65dc
ursystempass <jungw0npepsi@gmail.com>
2024-01-12 11:50:30 +0700

    feat: search, sweetalert & message

--- resources/views/componen/layout.blade.php
+++ resources/views/componen/layout.blade.php
@@ -61,12 +61,7 @@
         <!-- Navbar -->
         <nav>
             <i class='bx bx-menu'></i>
-            <form action="#">
-                <div class="form-input">
-                    <input type="search" placeholder="Search...">
-                    <button class="search-btn" type="submit"><i class='bx bx-search'></i></button>
-                </div>
-            </form>
+
             <input type="checkbox" id="theme-toggle" hidden>
             <label for="theme-toggle" class="theme-toggle"></label>
 
@@ -198,9 +193,10 @@
                 text: 'Apakah Anda yakin ingin keluar?',
                 icon: 'warning',
                 showCancelButton: true,
-                confirmButtonColor: '#18537a',
-                cancelButtonColor: '#d33',
+                confirmButtonColor: '#FF0000',
+                cancelButtonColor: '#0000FF',
                 confirmButtonText: 'Logout!',
+                reverseButtons: true
             }).then((result) => {
                 if (result.isConfirmed) {
                     window.location.href = "{{ route('logout') }}";
